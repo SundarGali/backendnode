0a7ee9151647e84f1253ba22232bae83a24aa7f3
prasanna sundar <sundar@MacBook-Pro.local>
2021-12-08 14:33:25 -0500

    make it better

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const server = http.createServer((req, res) => {
                     (err, content) => {
                                     
                                     if (err) throw err;
-                                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                                    
                                     res.end(content);
                         }
               );
